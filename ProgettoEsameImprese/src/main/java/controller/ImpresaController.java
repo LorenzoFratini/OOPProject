50f542aa8c4e42f54b94ed707a82d2a11809c746
Utente <Utente@192.168.1.4>
2019-06-12 13:26:32 +0200

    Aggiunte modifiche dentro la classe ImpresaController per la restituzione dei dati compresi fra due valori di riferimento

--- ProgettoEsameImprese/src/main/java/controller/ImpresaController.java
+++ ProgettoEsameImprese/src/main/java/controller/ImpresaController.java
@@ -21,23 +21,36 @@ public class ImpresaController {
 	private String fieldName;
 	private String operator;
 	private String value;
+	private String[] values=new String[2];
 	
 	@Autowired
 	ImpresaService impserv;
 	
-	ArrayList<Impresa> out1=new ArrayList<Impresa>();
-	ArrayList<Impresa> out2=new ArrayList<Impresa>();
+	//Definisco dei HashSet dove memorizzare le Collection che derivano dalle due query inserite
+	private ArrayList<Impresa> out1=new ArrayList<Impresa>();
+	private ArrayList<Impresa> out2=new ArrayList<Impresa>();
+	private ArrayList<Impresa> out3=new ArrayList<Impresa>();
+	private ArrayList<Impresa> out4=new ArrayList<Impresa>();
+	private HashSet<Impresa> hs1;
+	private HashSet<Impresa> hs2;
+	private HashSet<Impresa> hs3;
+	private HashSet<Impresa> hs4;
+	
 	
 	//Metodo per il parse della querystring
 	public void ParseQuery(String query) {
 		String[] token=query.split(":");
 		this.fieldName=token[0];
 		if(this.fieldName.equals("Dim")||this.fieldName.equals("dim") || this.fieldName.equals("CodAteco") || this.fieldName.equals("codAteco") || this.fieldName.equals("Descrizione") || this.fieldName.equals("descrizione")) {
-			this.operator=null; //non ha senso definite operatori matematici per le stringhe
+			this.operator="null"; //non ha senso definire operatori matematici per le stringhe
 			this.value=token[1];
 		} else {
 			this.operator=token[1];
-			this.value=token[2];
+			//Gestisco il caso in cui si passano come valori degli estremi di riferimento (vengono separati con delle virgole)
+			String appoggio[]=token[2].split(",");
+			for(int i=0;i<appoggio.length;i++) {
+			this.values[i]=appoggio[i];
+			}
 		}
 		
 	}
@@ -59,48 +72,103 @@ public class ImpresaController {
 				//Continuo il parse della Stringa in cui sono presenti i filtri
 				String query1=tokenquery[1];
 				String query2=tokenquery[2];
-				//System.out.println(logicalop+" -- "+query1+" -- "+query2);
 				//Faccio il parsing della prima query e restituisco una collezione contenente i dati che soddisfano i filtri
 				ParseQuery(query1);
-				//System.out.println(fieldName+" "+operator+" "+value);
-				out1=impserv.filterField(fieldName, operator, value);
-				if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {//se entro nell'if si parla di interi
-					out1= impserv.filterField(fieldName, operator, Integer.parseInt(value));
-				}else out1= impserv.filterField(fieldName, operator, value);
 				
+				//Separo il caso in cui inserisco come operatore $bt dagli altri casi banali
+				switch(operator) {
+					case "$bt": {
+						//if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {
+								//Considero l'operatore $bt come intersezione fra $gt e $lt
+								out1=impserv.filterField(fieldName, "$gt", Integer.parseInt(values[0]));
+								out3=impserv.filterField(fieldName, "$lt", Integer.parseInt(values[1]));	
+								hs1=new HashSet<Impresa>(out1);
+								hs3=new HashSet<Impresa>(out3);
+								hs1.retainAll(hs3);
+						
+					};break;
+				//Casi banali dove non inserisco operatori oppure inserisco operatori semplici	
+					default:  {
+						if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {//se entro nell'if si parla di interi
+							out1= impserv.filterField(fieldName, operator, Integer.parseInt(values[0]));
+							hs1=new HashSet<Impresa>(out1);
+						} else {
+							out1= impserv.filterField(fieldName, operator, value);
+							hs1=new HashSet<Impresa>(out1);
+						}
+					}
+				}
 				//Faccio il parsing della seconda query e restituisco una collezione contente i dati che soddisfano i filtri
-					ParseQuery(query2);
-				//System.out.println(fieldName+" "+operator+" "+value);
-					if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {//se entro nell'if si parla di interi
-						out2 = impserv.filterField(fieldName, operator, Integer.parseInt(value));//spiegata la conversione
-					}else out2= impserv.filterField(fieldName,operator,value);
+				ParseQuery(query2);
+
+				//Separo il caso in cui inserisco come operatore $bt dagli altri casi banali
+				switch(operator) {
+					case "$bt": {
+						//if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {
+						//Considero l'operatore $bt come intersezione fra $gt e $lt
+							out2=impserv.filterField(fieldName, "$gt", Integer.parseInt(values[0]));
+							out4=impserv.filterField(fieldName, "$lt", Integer.parseInt(values[1]));	
+							hs2=new HashSet<Impresa>(out2);
+							hs4=new HashSet<Impresa>(out4);
+							hs2.retainAll(hs4);
+						
+					};break;
+				//Casi banali dove non inserisco operatori oppure inserisco operatori semplici
+					default:  {
+						if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {//se entro nell'if si parla di interi
+							out2= impserv.filterField(fieldName, operator, Integer.parseInt(values[0]));
+							hs2=new HashSet<Impresa>(out2);
+						} else {
+							out2= impserv.filterField(fieldName, operator, value);
+							hs2=new HashSet<Impresa>(out2);
+						}
+					}
+					
+				}
 				
+				//Se l'operatore è un $or faccio la fusione delle due Collection
+				if(logicalop.equals("$or"))  {
+					hs1.addAll(hs2);
+					return hs1;
+				}
+				
+				//Se l'operatore è un $and faccio la fusione solo degli elementi comuni alle due Collection 
+				if(logicalop.equals("$and")) {
+					hs1.retainAll(hs2);
+					return hs1;
+				}
 			//Gestisco il caso in cui non ci sono operatori logici e quindi si ha una sola query in cui vi sono i filtri
 		     }else {
 		    	  logicalop=null;
 		    	  ParseQuery(query);
-				if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {//se entro nell'if si parla di interi
-						out1= impserv.filterField(fieldName, operator, Integer.parseInt(value));//spiegata la conversione
-					}else out1= impserv.filterField(fieldName, operator, value);
-				return out1;
+		    	  switch(operator) {
+					case "$bt": {
+						//if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {
+								out1=impserv.filterField(fieldName, "$gt", Integer.parseInt(values[0]));
+								out3=impserv.filterField(fieldName, "$lt", Integer.parseInt(values[1]));	
+								hs1=new HashSet<Impresa>(out1);
+								hs3=new HashSet<Impresa>(out3);
+								hs1.retainAll(hs3);
+						
+					};break;
+					
+					default:  {
+						if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {//se entro nell'if si parla di interi
+							out1= impserv.filterField(fieldName, operator, Integer.parseInt(values[0]));
+							hs1=new HashSet<Impresa>(out1);
+						} else {
+							out1= impserv.filterField(fieldName, operator, value);
+							hs1=new HashSet<Impresa>(out1);
+						}
+					}
+				}
+		    	  return hs1;
+				/*if(!(fieldName.equals("CodAteco")||fieldName.equals("codAteco")||fieldName.equals("dim")||fieldName.equals("Dim")|| fieldName.equals("Descrizione") || fieldName.equals("descrizione"))) {//se entro nell'if si parla di interi
+						out1= impserv.filterField(fieldName, operator, Integer.parseInt(value[0]));//spiegata la conversione
+					}else out1= impserv.filterField(fieldName, operator, value[0]);
+				return out1;*/
 		          
 		      }
-			
-			//Definisco due HashSet dove memorizzare le due Collection che derivano dalle due query inserite
-			HashSet<Impresa> hs = new HashSet<Impresa>(out1);
-			HashSet<Impresa> hs2 = new HashSet<Impresa>(out2);
-			
-			//Se l'operatore è un $or faccio la fusione delle due Collection
-			if(logicalop.equals("$or"))  {
-				hs.addAll(hs2);
-				return hs;
-			}
-			
-			//Se l'operatore è un $and faccio la fusione solo degli elementi comuni alle due Collection 
-			if(logicalop.equals("$and")) {
-				hs.retainAll(hs2);
-				return hs;
-			}
 			return null;
 		}	
 	}
